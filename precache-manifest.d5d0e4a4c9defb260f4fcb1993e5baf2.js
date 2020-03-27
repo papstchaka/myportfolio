b549fb3d2ae420e809473659bd41f5f113333266
papstchaka <alex.christoph.ac@gmail.com>
2020-03-27 20:55:34 +0100

    Updates

---
+++ precache-manifest.d5d0e4a4c9defb260f4fcb1993e5baf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42775ec1ca8fc3716d8ec741b2c41352",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "1cebfdd202a9890de980",
+    "url": "/myportfolio/static/css/main.fba464f6.chunk.css"
+  },
+  {
+    "revision": "c9b50b78fcbb2d086ab4",
+    "url": "/myportfolio/static/js/2.57a3283f.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/myportfolio/static/js/2.57a3283f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cebfdd202a9890de980",
+    "url": "/myportfolio/static/js/main.829cf710.chunk.js"
+  },
+  {
+    "revision": "6600aec0acac4ebef96a",
+    "url": "/myportfolio/static/js/runtime-main.4f0cea2f.js"
+  }
+]);
