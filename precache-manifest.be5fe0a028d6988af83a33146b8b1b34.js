85f77867e77bb2ad9aa6d3060fd4e8fc9fd68f89
papstchaka <alex.christoph.ac@gmail.com>
2020-10-03 10:48:19 +0200

    Updates

---
+++ precache-manifest.be5fe0a028d6988af83a33146b8b1b34.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda3fb598dea0dea785e14e152dee993",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "905e2dd901bc7babdbed",
+    "url": "/myportfolio/static/css/main.44c38837.chunk.css"
+  },
+  {
+    "revision": "fdc30403b6ec101a8018",
+    "url": "/myportfolio/static/js/2.6cba29c6.chunk.js"
+  },
+  {
+    "revision": "a7d6a1925d854d128f10ff8ad6f7f54b",
+    "url": "/myportfolio/static/js/2.6cba29c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "905e2dd901bc7babdbed",
+    "url": "/myportfolio/static/js/main.6b194ca6.chunk.js"
+  },
+  {
+    "revision": "6600aec0acac4ebef96a",
+    "url": "/myportfolio/static/js/runtime-main.4f0cea2f.js"
+  },
+  {
+    "revision": "89f0cc20bb35194ea97e1fe7a9bc2b21",
+    "url": "/myportfolio/static/media/Lebenslauf.89f0cc20.pdf"
+  },
+  {
+    "revision": "6f3224a2f67c186e856b851715e367c9",
+    "url": "/myportfolio/static/media/basic_ml.6f3224a2.jpg"
+  },
+  {
+    "revision": "b659c65984c163b09815835798705d7c",
+    "url": "/myportfolio/static/media/dash_test.b659c659.jpg"
+  },
+  {
+    "revision": "4113ae6bff8fb4fa4b92b175f6ab05df",
+    "url": "/myportfolio/static/media/django_test.4113ae6b.jpg"
+  },
+  {
+    "revision": "8cfcce9afa5d73e8c812bf03ea71ac53",
+    "url": "/myportfolio/static/media/explore-corona.8cfcce9a.jpg"
+  },
+  {
+    "revision": "1c687f16138c97c8080a6dfcb88cc4ca",
+    "url": "/myportfolio/static/media/profile.1c687f16.jpg"
+  },
+  {
+    "revision": "ef2e3f242d88b6f590af327f2d667e43",
+    "url": "/myportfolio/static/media/robofriends.ef2e3f24.jpg"
+  },
+  {
+    "revision": "909002d086552957570c86911a0b4120",
+    "url": "/myportfolio/static/media/simple_django_react_app.909002d0.jpg"
+  },
+  {
+    "revision": "9816f2ed3c60b92a1aef491194c6afc7",
+    "url": "/myportfolio/static/media/smartbrain.9816f2ed.jpg"
+  },
+  {
+    "revision": "e071cd3ce2d6f00d77661bd1f5355adc",
+    "url": "/myportfolio/static/media/smartq.e071cd3c.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/myportfolio/precache-manifest.8154bf979350318bf2b8328ac4d37406.js"
+  "/myportfolio/precache-manifest.be5fe0a028d6988af83a33146b8b1b34.js"
 );
 
 self.addEventListener('message', (event) => {
