fa18f6adbda25705cb77e5e19253965c0fca910e
papstchaka <alex.christoph.ac@gmail.com>
2021-08-13 10:53:43 +0200

    updated Porjects' layout

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ProjectManager from './ProjectManager';
 import Fade from 'react-reveal/Fade';
+import Accordion from 'react-bootstrap/Accordion';
+import ReactHtmlParser from 'react-html-parser';
 
 function ProjectList({projects}) {
     return (
@@ -10,19 +12,50 @@ function ProjectList({projects}) {
             <Fade>
               <h2 className="section-title dark-blue-text">
                 Projects
+                <div class="button-bar">
+                  {
+                    projects.reverse().map((project, i) => {
+                      return(
+                        <a className="cta-btn cta-btn--hero" href={"#"+project.year} style={{"margin":"0px 10px 0px 10px"}}>{project.year}</a>
+                      )
+                    })
+                  }
+                </div>
               </h2>
             </Fade>
             { 
-              projects.map((project,i) => {
-                return (
-                  <ProjectManager 
-                  key={projects[i].name} 
-                  name={projects[i].name} 
-                  data={projects[i].data}
-                  description={projects[i].description}
-                  wrapped={projects[i].wrapped}
-                  />
-                  );
+              projects.reverse().map((project,i) => {
+                return (    
+                  <div id={project.year} style={{"paddingTop":"10px"}}>         
+                    <div className="wrapper years">
+                      <Accordion defaultActiveKey="1">
+                        <Accordion.Toggle as="H3" variant="link" eventKey="0" >
+                            <h3 className="project-wrapper__text-title accordion-button">{project.year}</h3>
+                            <div className="project-wrapper__text">
+                                <p className="mb-4" style={{"marginLeft":"2px"}}>{ReactHtmlParser(project.description)}</p>
+                            </div>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="0">
+                            <div className="accordion-content">
+                            {
+                              project.data.map((data,i) => {
+                                return (
+                                        <ProjectManager 
+                                            key={data.name} 
+                                            name={data.name} 
+                                            data={data.data}
+                                            description={data.description}
+                                            wrapped={data.wrapped}
+                                        />
+                                      )
+                              })
+                            }
+                            </div>
+                        </Accordion.Collapse>
+                      </Accordion>
+                    </div>
+                  </div>
+                );
               })
             }
           </div>
