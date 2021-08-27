e5830f867cfea007d4ce866fb8350a1e5968f340
papstchaka <alex.christoph.ac@gmail.com>
2021-08-27 10:28:15 +0200

    Updates

---
+++ precache-manifest.19cad535d54878b268a378fe4c149d3b.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e6c4fa56e9ab7fdd2abd0621a80fdd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "753b7f0fcfff62a99406",
+    "url": "/static/css/main.8a7ec017.chunk.css"
+  },
+  {
+    "revision": "f6953b4f31a6f3da131b",
+    "url": "/static/js/2.f5519a66.chunk.js"
+  },
+  {
+    "revision": "a7d6a1925d854d128f10ff8ad6f7f54b",
+    "url": "/static/js/2.f5519a66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "753b7f0fcfff62a99406",
+    "url": "/static/js/main.411414c7.chunk.js"
+  },
+  {
+    "revision": "06049379b30b7a761732",
+    "url": "/static/js/runtime-main.e9342163.js"
+  },
+  {
+    "revision": "a58ae9fef07bb4cc8c8726bbdcbfc5e4",
+    "url": "/static/media/CV.a58ae9fe.pdf"
+  },
+  {
+    "revision": "0f0cc4a8d214f5b9351b504a7f81e9c0",
+    "url": "/static/media/CV_show.0f0cc4a8.pdf"
+  },
+  {
+    "revision": "0b155c21182fb4240b02e26497fb03ba",
+    "url": "/static/media/barnsley_fern.0b155c21.webp"
+  },
+  {
+    "revision": "3ff2e1b9c2fe948bf35aed14cf33aa03",
+    "url": "/static/media/basic_ml.3ff2e1b9.webp"
+  },
+  {
+    "revision": "29d82e465fd6a1f2f8eadbeda476de12",
+    "url": "/static/media/dash_test.29d82e46.webp"
+  },
+  {
+    "revision": "91a9777ec08322a5600d0a0a4f846313",
+    "url": "/static/media/django_test.91a9777e.webp"
+  },
+  {
+    "revision": "1b451cc0390def302fc3afc1d3e0f660",
+    "url": "/static/media/explore-corona.1b451cc0.webp"
+  },
+  {
+    "revision": "9f1c3f8a2984074425325ccd5a18d0c2",
+    "url": "/static/media/hpc_parallel.9f1c3f8a.webp"
+  },
+  {
+    "revision": "4a369c938ef4ffdffa9b71f752f042b5",
+    "url": "/static/media/mnist.4a369c93.webp"
+  },
+  {
+    "revision": "94adfd80f1e806ab23c73f2affa35e73",
+    "url": "/static/media/profile.94adfd80.webp"
+  },
+  {
+    "revision": "55d079d0a06ffbb2c84f1887cdf05b4c",
+    "url": "/static/media/resulting_paths_asyncvi.55d079d0.webp"
+  },
+  {
+    "revision": "ce8190d4ab7108e62b58ece0a50bd7b2",
+    "url": "/static/media/robofriends.ce8190d4.webp"
+  },
+  {
+    "revision": "c9ad3e848b95092d6b4ad9320d2b8308",
+    "url": "/static/media/simple_django_react_app.c9ad3e84.webp"
+  },
+  {
+    "revision": "2be6b2649bafb77e81592678c14015e7",
+    "url": "/static/media/smartbrain.2be6b264.webp"
+  },
+  {
+    "revision": "78fb16a1a4dfbb3fd440f39f96c20f08",
+    "url": "/static/media/smartq.78fb16a1.webp"
+  },
+  {
+    "revision": "8c66b09e3794a8fb1c0fb3c67430bdc4",
+    "url": "/static/media/sort_music.8c66b09e.webp"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d6235969ba427a1cd2cfbc4c055ae960.js"
+  "/precache-manifest.19cad535d54878b268a378fe4c149d3b.js"
 );
 
 self.addEventListener('message', (event) => {
