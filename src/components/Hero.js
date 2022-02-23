162ec668c5fd0dd0858d5a644365b195b0726754
papstchaka <alex.christoph.ac@gmail.com>
2022-02-23 17:26:33 +0100

    added navigation bar on top of website

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import Fade from 'react-reveal/Fade';
-import { animateScroll } from 'react-scroll'
+import { animateScroll, Link } from 'react-scroll'
 import TypeAnimation from 'react-type-animation'
 
 function Hero({hero}) {
     return (
         <div id="hero" className="jumbotron">
-            <div className="container">
-                <p>
-                    <a className="back-to-top">
-                        <i className="fa fa-angle-down fa-2x load-hidden" aria-hidden="true"></i>
-                    </a>
-                </p>
-            </div>
+            <nav className="navnavbar">
+                <div className="divnavbar">
+                    <p className="hero-cta pnavbar">
+                        <Link className="cta-btn cta-btn--resume anavbar" to="about" spy={true} smooth={true}>About Me</Link>
+                    </p>
+                    <p className="hero-cta pnavbar">
+                        <Link className="cta-btn cta-btn--resume anavbar" to="projects" spy={true} smooth={true}>My Projects</Link>
+                    </p>
+                    <p className="hero-cta pnavbar">
+                        <Link className="cta-btn cta-btn--resume anavbar" to="contact" spy={true} smooth={true}>Contact Me</Link>
+                    </p>
+                    <p className="hero-cta pnavbar">
+                        <Link className="cta-btn cta-btn--resume anavbar" to="footer" spy={true} smooth={true}>Social Media</Link>
+                    </p>
+                </div>
+            </nav>
             <div className="container">
                 <Fade left>
                     <h1 className="hero-title">
